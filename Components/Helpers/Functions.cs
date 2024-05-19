0c83d4b7ea0432e5e2d05fc35604e858c953ef33
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2024-05-19 17:05:38 -0400

    need to implement search feature

---
+++ Components/Helpers/Functions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.JSInterop;
+using OfficeOpenXml;
+using System.Text;
+using WebServer_Proof_of_concept.Components.Models;
+
+namespace WebServer_Proof_of_concept.Components.Helpers
+{
+    public static class Functions
+    {
+        public static async Task ExportToExcel(IJSRuntime jsRuntime, int activeTab, IEnumerable<object> data)
+        {
+            if (data == null) return;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Data");
+                var properties = data.First().GetType().GetProperties();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = properties[i].Name;
+                }
+
+                var index = 2;
+                foreach (var item in data)
+                {
+                    for (int i = 0; i < properties.Length; i++)
+                    {
+                        worksheet.Cells[index, i + 1].Value = properties[i].GetValue(item)?.ToString();
+                    }
+                    index++;
+                }
+
+                var excelData = package.GetAsByteArray();
+                var fileName = activeTab == 1 ? "Users.xlsx" : "Businesses.xlsx";
+                var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                await jsRuntime.InvokeVoidAsync("downloadFile", fileName, contentType, excelData);
+            }
+        }
+
+        public static async Task ExportToCsv(IJSRuntime jsRuntime, int activeTab, IEnumerable<object> data)
+        {
+            if (data == null) return;
+
+            var csvBuilder = new StringBuilder();
+            var properties = data.First().GetType().GetProperties();
+            csvBuilder.AppendLine(string.Join(",", properties.Select(p => p.Name)));
+
+            foreach (var item in data)
+            {
+                var values = properties.Select(p => p.GetValue(item)?.ToString());
+                csvBuilder.AppendLine(string.Join(",", values));
+            }
+
+            var csvData = Encoding.UTF8.GetBytes(csvBuilder.ToString());
+            var fileName = activeTab == 1 ? "Users.csv" : "Businesses.csv";
+            var contentType = "text/csv";
+
+            await jsRuntime.InvokeVoidAsync("downloadFile", fileName, contentType, csvData);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # WebServer_Proof_of_concept
+
+## About
+This is a proof of concept for a web server application. In this example there are 4 pages:
+* Home: example of an entry point into an app
+* Create New User/Business: example of using the API's POST routes to create new data as well as using a form in C#.
+* Data Tables: listing page and an example of how the GET route of the API would work. The table also has functionality for modal generation on a table column, different tabs, exporting to CSV/Excel, refreshing, searching, sorting, and filtering
+
+There is more to be done with these pages (authentication, session management, etc) and more research needs to be done as to how we can spin up an application on our servers.
+
+
+## Folders
+* wwwroot: root webserver
+* components: parts of the pages/assets
+* Assets: sub pages (Ex: modals)
+* Helpers: helper functions that can be used by any page
+* Layout: main background and nav bar
+* Models: classes that page will interact with
+* Pages: pages that will be rendered
+
+## Files
+* _Imports.razor: all the imports that will be used globally
+* App.razor: main app that combines all the routes
+* Routes.razor: router for all the pages navigation
+* Program.cs: main launcher for the web app
